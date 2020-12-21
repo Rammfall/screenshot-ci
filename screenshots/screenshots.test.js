b1d7d4085020521a771d19dc86db388463d4d5ca
Bohdan Onatskyi <15bogdan17@gmail.com>
2020-12-22 00:02:52 +0200

    Change

--- screenshots/screenshots.test.js
+++ screenshots/screenshots.test.js
@@ -3,30 +3,40 @@ const puppeteer = require('puppeteer');
 const resolutions = require('./config/resolutions');
 const { pages, url } = require('./config/pages');
 
-describe.each(pages)('Page %p',  (currentPage) => {
+describe('Screenshots tests', () => {
   let browser;
-  let page;
 
   beforeAll(async () => {
     browser = await puppeteer.launch();
-    page = await browser.newPage();
-
-    await page.goto(`${url}/${currentPage}`);
   });
 
-  test.each(resolutions)('Test screenshot on %i width, height %i > %s', async (width, height, resolution) => {
-    await page.setViewport({
-      width,
-      height
-    });
-    console.log(2);
-    const image = await page.screenshot({fullPage: true});
 
-    expect(image).toMatchImageSnapshot({
-      customSnapshotsDir: `./screenshots/__snapshots__/${currentPage}`,
-      customDiffDir: `./screenshots/__snapshots__/${currentPage}/diffs`,
-      customSnapshotIdentifier: `${resolution}-${width}`
+  describe.each(pages)('Page %p', ( currentPage ) => {
+    let page;
+
+    beforeAll(async () => {
+      page = await browser.newPage();
+
+      await page.goto(`${url}/${currentPage}`);
     });
+
+    test.each(resolutions)('Test screenshot on %i width, height %i > %s', async ( width, height, resolution ) => {
+      await page.setViewport({
+        width,
+        height
+      });
+      const image = await page.screenshot({ fullPage: true });
+
+      expect(image).toMatchImageSnapshot({
+        customSnapshotsDir: `./screenshots/__snapshots__/${currentPage}`,
+        customDiffDir: `./screenshots/__snapshots__/${currentPage}/diffs`,
+        customSnapshotIdentifier: `${resolution}-${width}`
+      });
+    });
+
+    afterAll(async () => {
+      await page.close();
+    })
   });
 
   afterAll(async () => {
