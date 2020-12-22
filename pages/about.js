8a0fa4b96d7bf6b51abcd3ae7f7600821306b4a4
Bohdan Onatskyi <15bogdan17@gmail.com>
2020-12-22 15:08:31 +0200

    Update texts

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AboutPage = () => (
-  <div>About page</div>
+  <div>About pageAbout pageAbout pageAbout pageAbout pageAbout pageAbout pageAbout pageAbout page About pageAbout pageAbout pageAbout pageAbout pageAbout page</div>
 );
 
 export default AboutPage;
